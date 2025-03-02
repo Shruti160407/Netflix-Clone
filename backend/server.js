a9440aa240acf327297f95cc6e1befe95560567f
Shruti160407 <shrutichourasia1607@gmail.com>
2025-03-02 23:21:01 +0530

    user.model file added and updated

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 
 import authRoutes from "./routes/auth.route.js";
 import { ENV_VARS } from './config/envVars.js';
+import { connectDB } from './config/db.js';
 
 
 
@@ -16,5 +17,6 @@ app.use("/api/v1/auth",authRoutes)
 
 app.listen(PORT, () => {
     console.log('Server started at http://localhost:' + PORT);
+    connectDB();
 })
 
