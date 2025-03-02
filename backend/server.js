614d9b7020f3b818965d40a1c8195dab241ab2d5
Shruti160407 <shrutichourasia1607@gmail.com>
2025-03-02 13:42:01 +0530

    backend updating

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,16 @@
 import express from 'express';
 const app = express(); 
 
+app.get("/api/signup", (req, res) => {
+    res.send("Signup route");
+});
+app.get("/api/login", (req, res) => {
+    res.send("Login route");
+});
+app.get("/api/logout", (req, res) => {
+    res.send("Logout route");
+});
+
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
 })
